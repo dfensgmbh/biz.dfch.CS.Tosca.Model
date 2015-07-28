23a550a2131286bb5c594ecac614a16c934cc305
Ronald Rink <ronald.rink@d-fens.net>
2015-07-28 16:34:35 +0200

    added projects for Model and Model.Tests

---
+++ src/biz.dfch.CS.Tosca.Model.Tests/ModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Tosca.Model.Tests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        [Timeout(1000)]
+        public void SleepingThrowsTimeoutException()
+        {
+            System.Threading.Thread.Sleep(5000);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DividingByZeroThrowsDivideByZeroException()
+        {
+            var x = 1;
+            var y = 0;
+            var result = x / y;
+            Assert.AreNotEqual(0, result);
+        }
+    }
+}
---
+++ src/biz.dfch.CS.Tosca.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biz.dfch.CS.Tosca.Model
+{
+    public class Class1
+    {
+    }
+}
