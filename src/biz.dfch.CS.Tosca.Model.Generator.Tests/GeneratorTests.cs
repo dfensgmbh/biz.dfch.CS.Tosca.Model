5b98b41f1ffb4222b1d0a40c6a5b2892bf14c254
Ronald Rink <ronald.rink@d-fens.net>
2015-07-28 16:55:00 +0200

    added Generator project with XSD TOSCA schema

---
+++ src/biz.dfch.CS.Tosca.Model.Generator.Tests/GeneratorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Tosca.Model.Generator.Tests
+{
+    [TestClass]
+    public class GeneratorTests
+    {
+        [TestMethod]
+        [WorkItem(1)]
+        public void LoadingSchemaReturnsXmlDocument()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
