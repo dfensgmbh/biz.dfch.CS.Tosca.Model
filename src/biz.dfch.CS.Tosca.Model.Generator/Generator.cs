f3d52764e8ef84714c5c2c0b37ce6822ce2df3bb
Ronald Rink <ronald.rink@d-fens.net>
2015-07-29 00:06:24 +0200

    add command how to use XSD

--- src/biz.dfch.CS.Tosca.Model.Generator/Generator.cs
+++ src/biz.dfch.CS.Tosca.Model.Generator/Generator.cs
@@ -8,5 +8,8 @@ namespace biz.dfch.CS.Tosca.Model.Generator
 {
     public class Generator
     {
+        // currently we use XSD to convert the schema to POCO classes
+        // Set-Alias XSD C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\x64\xsd.exe
+        // xsd .\TOSCA-v1.0.xsd  /classes /f /namespace:biz.dfch.CS.Tosca.Model
     }
 }
